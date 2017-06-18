91f7c1ff9790f00480ec5e77dcc61e7230529199
AkhilRaja <p.akhilraja@gmail.com>
2017-06-18 17:44:05 +0530

    Recycler View Implemented

---
+++ app/src/main/java/com/example/akhilraja/bakingapp/Activities/ActivityFragment.java
@@ -0,0 +1,8 @@
+package com.example.akhilraja.bakingapp.Activities;
+
+/**
+ * Created by AkhilRaja on 16/06/17.
+ */
+
+public class ActivityFragment {
+}
---
+++ app/src/main/java/com/example/akhilraja/bakingapp/Adapter/MyAdapter.java
@@ -0,0 +1,8 @@
+package com.example.akhilraja.bakingapp.Adapter;
+
+/**
+ * Created by AkhilRaja on 17/06/17.
+ */
+
+public class MyAdapter {
+}
